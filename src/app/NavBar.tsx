b283adbb61476759b122e5cb73a14a9d1510b9aa
Nayan <52083057+LaterIdiot@users.noreply.github.com>
2023-07-20 16:41:17 -0400

    fix: pages action

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -3,17 +3,16 @@
 import Link from "next/link";
 import styles from "./NavBar.module.css";
 import { usePathname } from "next/navigation";
-import { getBasePath } from "@/scripts";
 
 export default function NavBar() {
   const pathname = usePathname();
 
   return (
     <nav className={styles.nav}>
-      <Link className={`${styles.link} ${pathname === `${getBasePath()}/` ? styles.active : ""}`} href="/">
+      <Link className={`${styles.link} ${pathname === "/" ? styles.active : ""}`} href="/">
         Home
       </Link>
-      <Link className={`${styles.link} ${pathname === `${getBasePath()}/chess/` ? styles.active : ""}`} href="/chess">
+      <Link className={`${styles.link} ${pathname === "/chess/" ? styles.active : ""}`} href="/chess">
         Chess
       </Link>
     </nav>
