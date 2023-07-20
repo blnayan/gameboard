212e5108cb84525a897a96d45000dda0d7a2031d
Nayan <52083057+LaterIdiot@users.noreply.github.com>
2023-07-20 16:30:02 -0400

    fix: active page highlighting

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -3,16 +3,17 @@
 import Link from "next/link";
 import styles from "./NavBar.module.css";
 import { usePathname } from "next/navigation";
+import { getBasePath } from "@/scripts";
 
 export default function NavBar() {
   const pathname = usePathname();
 
   return (
     <nav className={styles.nav}>
-      <Link className={`${styles.link} ${pathname === "/" ? styles.active : ""}`} href="/">
+      <Link className={`${styles.link} ${pathname === `${getBasePath()}/` ? styles.active : ""}`} href="/">
         Home
       </Link>
-      <Link className={`${styles.link} ${pathname === "/chess/" ? styles.active : ""}`} href="/chess">
+      <Link className={`${styles.link} ${pathname === `${getBasePath()}/chess/` ? styles.active : ""}`} href="/chess">
         Chess
       </Link>
     </nav>
